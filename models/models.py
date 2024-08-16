09fed32b1063a0ebbcac106c176b2963a1b078b0
602494 <144184882+602494@users.noreply.github.com>
2024-08-16 16:30:20 +0900

    Update models.py

--- models/models.py
+++ models/models.py
@@ -8,12 +8,9 @@ def model_(params, device):
     par['block'] = params.bc
     par['weight'] = params.weight
 
-    if par['model'] == "densenet":
-        mod = densenet.DenseNet(block_config=par['block'], device=device)
-        if par['weight']:
-            densenet._load_state_dict(mod, densenet.DenseNet121_Weights.DEFAULT, False)
-    elif par['model'] == "resnet":
-        mod = resnet.ResNet(block=resnet.Bottleneck, layers=par['block'])
-        if par['weight']:
-            mod.load_state_dict(resnet.Resnet50_Weights.DEFAULT, False)
+ 
+    mod = resnet.ResNet(block=resnet.Bottleneck, layers=par['block'])
+    if par['weight']:
+        mod.load_state_dict(resnet.Resnet50_Weights.DEFAULT, False)
+        
     return mod
